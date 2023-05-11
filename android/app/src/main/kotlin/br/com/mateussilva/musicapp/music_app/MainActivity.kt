7202c281b404cd5497cae2cfda2f1c3061bc1ffb
Mateus Silva <mateusilva767@gmail.com>
2023-05-11 13:43:58 -0300

    Project created

---
+++ android/app/src/main/kotlin/br/com/mateussilva/musicapp/music_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.mateussilva.musicapp.music_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/app/music_app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MusicAppColors {
+  MusicAppColors._();
+
+  static Color get primaryColors => const Color(0xFF000000);
+  static Color get secondaryColor => const Color(0xFFFFFFFF);
+  static Color get tertiaryColor => const Color(0xFF779933);
+  static Color get errorColor => const Color(0xFFdc3545);
+}
